b014ce268dc02e824779b563185de7a5f823e61e
nhcarrigan <nhcarrigan@gmail.com>
2020-08-26 17:25:20 -0700

    Centered modal

--- src/question.js
+++ src/question.js
@@ -10,7 +10,13 @@ const highlight = (id) => {
 const showAnswer = () => {
   const validate = document.getElementsByClassName("highlighted");
   if (!validate.length) return alert("Please select an answer!");
-  document.getElementById("answer-modal").classList.add("shown");
+  const modal = document.getElementById("answer-modal");
+  modal.classList.add("shown");
+  const marginValue = Math.max(
+    0,
+    (document.body.clientHeight - modal.style.height) / 2
+  );
+  modal.setAttribute("style", `margin-top: ${marginValue}px`);
 };
 
 const hideAnswer = () => {
@@ -20,11 +26,11 @@ const hideAnswer = () => {
 const formatChat = () => {
   const chats = document.getElementsByClassName("chatbox");
   for (let i = 0; i < chats.length; i++) {
-    console.log(chats[i].innerHTML)
+    console.log(chats[i].innerHTML);
     chats[i].innerHTML = chats[i].innerHTML.replace(
       /^[\s]*(.*): (.*)/gm,
       "<img class='pfp' src='../../public/img/disc.jpg'><span class='username'>$1</span><span class='chat-content'>$2</span>"
     );
-    console.log(chats[i].innerHTML)
+    console.log(chats[i].innerHTML);
   }
 };
